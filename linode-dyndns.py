ed1fe0e44b8c923a8898c6089a3d4240da087d5f
Meethune Bhowmick <meethune@gmail.com>
2011-07-12 10:27:42 -0400

    Don't just trust one source. Fetch from multiple sources then verify.
    Signed-off-by: Meethune Bhowmick <meethune@gmail.com>

--- linode-dyndns.py
+++ linode-dyndns.py
@@ -20,19 +20,28 @@ from StringIO import StringIO
 APIKEY = 'ENTER API KEY HERE'
 DOMAIN = 'YOUR DOMAIN HERE'
 RECORD = 'HOST TO BE UPDATED'
-CHECKIP = "http://automation.whatismyip.com/n09230945.asp"
+CHECKIP = { "http://automation.whatismyip.com/n09230945.asp" ,
+            "http://checkip.dyndns.org:8245/" }
 
 def get_external_ip():
     """ Return the current external IP. """
-    print "Fetching external IP from: %s" % CHECKIP
-    html = StringIO()
-    curl = pycurl.Curl()
-    curl.setopt(curl.URL, CHECKIP)
-    curl.setopt(curl.WRITEFUNCTION, html.write)
-    curl.perform()
-    curl.close()
-    external_ip = html.getvalue().strip()
-    return external_ip
+    external=[]
+    for each in CHECKIP:
+        html = StringIO()
+        curl = pycurl.Curl()
+        curl.setopt(curl.URL, each)
+        curl.setopt(curl.WRITEFUNCTION, html.write)
+        curl.perform()
+        curl.close()
+        external.append(re.findall('[0-9.]+', html.getvalue())[0])
+        print "%s says your IP is: %s" % ( each, external[len(external)-1] )
+    # Unique Sort List to ensure both sites are saying the same thing
+    external_ip = sorted(set(external))
+    if len(external_ip) > 1:
+        print "Someone is Lying!"
+        return false
+    else:
+        return external_ip[0]
 
 def set_dns_target(utarget, udomain=DOMAIN, urecord=RECORD):
     """ Update the domain's DNS record with the specified target. """
@@ -58,4 +67,7 @@ def set_dns_target(utarget, udomain=DOMAIN, urecord=RECORD):
             print "Error: Domain %s not found." % udomain
 
 if __name__ == '__main__':
-    set_dns_target(get_external_ip(), DOMAIN, RECORD)
+    external_ip = get_external_ip()
+    if external_ip:
+        print "set_dns_target(%s, DOMAIN, RECORD)" % external_ip
+        #set_dns_target(external_ip, DOMAIN, RECORD)
